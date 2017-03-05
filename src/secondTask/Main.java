713080d1d40d09d765b8ce260da942a32ef09a7b
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-05 21:49:38 +0300

    p.s output added in main

--- src/secondTask/Main.java
+++ src/secondTask/Main.java
@@ -1,10 +1,19 @@
 package secondTask;
 
+import java.util.Iterator;
+
 /**
  * Created by Алексей on 05.03.2017.
  */
 public class Main {
     public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            Cat.cats.add(new Cat("Cat# " + i));
+        }
 
+        Iterator<Cat> iter = Cat.cats.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next().catName);
+        }
     }
 }
