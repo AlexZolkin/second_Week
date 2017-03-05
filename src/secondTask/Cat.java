5a7fcbea0a48c7882ff2b80e40141435aa236e13
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-05 21:34:55 +0300

    adding main() and finishing Cat class

--- src/secondTask/Cat.java
+++ src/secondTask/Cat.java
@@ -1,7 +1,16 @@
 package secondTask;
 
+import java.util.ArrayList;
+
 /**
  * Created by Алексей on 05.03.2017.
  */
 public class Cat {
+    private String catName;
+    public static ArrayList<Cat> cats = new ArrayList<Cat>();
+
+    public Cat(String catName){
+        this.catName = catName;
+    }
+
 }
---
+++ src/secondTask/Main.java
@@ -0,0 +1,10 @@
+package secondTask;
+
+/**
+ * Created by Алексей on 05.03.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
