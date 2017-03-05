404357ccc15958f1f667c8e8a530799120cd3cf2
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-05 21:46:36 +0300

    tests finished, task is done

--- src/secondTask/Cat.java
+++ src/secondTask/Cat.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by Алексей on 05.03.2017.
  */
 public class Cat {
-    private String catName;
+    public String catName;
     public static ArrayList<Cat> cats = new ArrayList<Cat>();
 
     public Cat(String catName){
--- tests/CatTest.java
+++ tests/CatTest.java
@@ -1,5 +1,23 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import secondTask.Cat;
+
 /**
  * Created by Алексей on 05.03.2017.
  */
+
+
 public class CatTest {
+    @Before
+    public void setUp(){
+        for(int i=0; i<10; i++){
+            Cat.cats.add(new Cat("Cat# " + i));
+        }
+    }
+
+    @Test
+    public void catsTest(){
+        Assert.assertEquals("Cat# 1",Cat.cats.get(1).catName);
+    }
 }
