09a5612deca7c98eefa5f130d26d6834a6556adf
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-15 01:57:08 +0300

    +refactoring

--- tests/BookTaskTest.java
+++ tests/BookTaskTest.java
@@ -16,30 +16,30 @@ public class BookTaskTest {
         for(int i=0;i<10;i++){
             switch (i % 3){
                 case 0:
-                    new Catalog("Catalog #" + i,"Author #" + i, i + 1950).add_Book();
+                    new Catalog("Catalog #" + i,"Author #" + i, i + 1950).addBook();
                     break;
                 case 1:
-                    new Encyclopedia("Encyclopedia #" + i,"Author #" + i, i + 1950).add_Book();
+                    new Encyclopedia("Encyclopedia #" + i,"Author #" + i, i + 1950).addBook();
                     break;
                 case 2:
-                    new PeriodicPrint("PeriodicPrint #" + i,"Author #" + i, i + 1950).add_Book();
+                    new PeriodicPrint("PeriodicPrint #" + i,"Author #" + i, i + 1950).addBook();
                     break;
             }
         }
     }
     @Test
     public void testDeleteBook(){
-        books.get(1).delete_Book();
+        books.get(1).deleteBook();
         Assert.assertEquals("PeriodicPrint",((PeriodicPrint)books.get(1)).info().split(" ")[0]);
     }
 
     @Test
     public void testAddBook(){
         //new Catalog("Const 1","Const 1",1).add_Book();
-        new Catalog("Const 1", "Const 1", 1).add_Book("Const 2", "Const 2", 2);
+        new Catalog("Const 1", "Const 1", 1).addBook("Const 2", "Const 2", 2);
         Catalog tmp = (Catalog) books.get(10);
 
-        Assert.assertEquals("Const 2 ",tmp.get_Book().split("-")[0]);
+        Assert.assertEquals("Const 2 ",tmp.getBook().split("-")[0]);
 
     }
 }
